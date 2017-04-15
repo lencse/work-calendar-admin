900efa51309163fd7d0aa20402870a7f577ec3af
Levente Löki <leventeloki@gmail.com>
2017-04-15 03:33:01 +0200

    Day types into store

---
+++ frontend/Store/DayType.ts
@@ -0,0 +1,8 @@
+export default class User {
+    
+    public id: string;
+    public isRestDay: boolean;
+    public key: string;
+    public name: string;
+
+}
--- frontend/Store/State.ts
+++ frontend/Store/State.ts
@@ -1,18 +1,38 @@
 import User from './User';
+import DayType from './DayType';
 import { Deserializer } from 'ts-jsonapi';
 import { assign } from 'lodash';
 
-const deserializer  = new Deserializer();
+const deserializer  = new Deserializer({
+    keyForAttribute: 'camelCase'
+});
 
 export default class State {
     
     public user: User = new User();
+    public dayTypes: DayType[] = [];
 
     public static getInitialState(): State {
         return new State();
     }
 
     public load(): Promise<any> {
+        return this.loadUser().then((state) => {
+            return state.loadDayTypes();
+        })
+    }
+
+    private loadDayTypes(): Promise<any> {
+        return fetch('/api/day-type', { credentials: 'include' })
+            .then((resp) => {
+                return resp.json().then((data) => {
+                    let dayTypes = deserializer.deserialize(data);
+                    return Promise.resolve(assign(this, {dayTypes: dayTypes}));
+                });
+            });
+    }
+
+    private loadUser(): Promise<any> {
         return fetch('/api/user/me', { credentials: 'include' })
             .then((resp) => {
                 return resp.json().then((data) => {
